71b8b6d3adc0555b728472d94f50b03afd029fd4
NairbCode <brayancasta.m@gmail.com>
2025-11-28 18:39:31 -1200

    Se agrego codigo a los siguientes archivos index.html, style.css y app.js.
    - Se agrego Estructura al index.html.
    - Se agregaron algunos estilo en el archivo style.css.
    
    - Por ultimo se agrego la funcion principal del sistema, iniciarSistema().

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+/**VARIABLES QUE SON ARRAYS Y OBJETOS QUE SE NECESITARAN PARA ALGUNAS FUNCIONES */
+/**ESTA ARRAY CON OBJETOS DENTRO SERVIRA PARA LAS COMIDAD QUE HAY DISPONIBLES */
+let menu = [
+    {nombre: "Hamburguesa", precio: 15000, tiempo: 10},
+    {nombre: "Gaseosa", precio: 3000, tiempo: 1},
+    {nombre: "Papas fritas", precio: 6000, tiempo: 8}
+];
+
+/**ESTA ARRAY EN PARA LAS MESAS */
+let mesas = [[], [], [], []]
+
+
+/**FUNCION PRINCIPAL INICIAR EL SISTEMA */
+/**=================================== */
+function iniciarSistema() {
+    let opciones;
+
+    /**DO WHILE POR SI ESCRIBE ALGO DE LO CONTRARIO QUE SE MUESTRA*/
+    do{
+
+        /**EL PROMPT CON LAS OPCIONES */
+        opciones = prompt(
+            "BIENVENIDO AL MENU PRINCIPAL\n" +
+            "======================\n" +
+            "1. Hacer un pedido.\n" +
+            "2. Ver estado de preparacion.\n" +
+            "3. Pagar la cuenta.\n" +
+            "4. Modo administrador.\n" +
+            "5. Salir.\n\n" +
+            "Seleccione una opcion: "
+        );
+
+        /**DIFERENTES OPCIONES, DEPENDIENDO LO QUE INGRESE EL USUARIO*/
+        switch (opciones) {
+            case "1":
+                hacerPedido();
+                break;
+            case "2":
+                verEstado();
+                break;
+            case "3":
+                pagarCuenta();
+                break;
+            case "4":
+                modoAdmin();
+                break;
+            case "5":
+                alert("Saliendo del sistema...");
+                break;
+            default:
+                alert("Por favor ingrese una opcion valida.");
+        }
+
+    } while (opciones !== "5");
+
+}
