21f94618db146024328de52af6cff422c32ded24
Santiago Ruiz Arroyave <ruiz.santiago1622@gmail.com>
2025-11-30 15:12:35 -0500

    Se arreglo el modo administrador debido a problemas de por nombrarlas diferente, ademas de esto se agrego la funcion de entrar al modo administrador

--- app.js
+++ app.js
@@ -61,11 +61,7 @@ function iniciarSistema() {
 //  SECCIÓN 1 SANTRIAGO: VALIDACIÓN DE CLAVE
 
 // Contraseña de acceso
-const CLAVE_CORRECTA = "1234";
 
-// Seleccionamos elementos del HTML
-const inputClave = document.querySelector('input[type="password"]');
-const boton = document.querySelector('button');
 
 // Crear el mensaje de error
 const mensajeError = document.createElement("p");
@@ -74,21 +70,57 @@ mensajeError.style.color = "red";
 mensajeError.style.display = "none";
 document.querySelector("main").appendChild(mensajeError);
 
-// Evento del botón
-boton.addEventListener("click", (e) => {
-  e.preventDefault(); // evitar recarga
-    verificarClave();
-    });
+
 
     // Función para verificar la clave
-    function verificarClave() {
+    function modoAdmin() {
     const claveIngresada = inputClave.value;
+    const CLAVE_CORRECTA = "1234";
+
+    // Seleccionamos elementos del HTML
+    const inputClave = document.querySelector('input[type="password"]');
+    const boton = document.querySelector('button');
 
     if (claveIngresada === CLAVE_CORRECTA) {
         alert("✅ Acceso concedido ✅");
         mensajeError.style.display = "none";
-        modoAdministrador(); // Entrar al modo administrador
+        modoAdmin(); // Entrar al modo administrador
     } else {
         mensajeError.style.display = "block";
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//  SECCIÓN 3 Santiago : MODO ADMINISTRADOR
+
+
+function modoAdmin() {
+    let opcion;
+    do {
+        opcion = prompt(
+        " MODO ADMINISTRADOR\n" +
+        "1. Agregar producto\n" +
+        "2. Modificar producto\n" +
+        "3. Eliminar producto\n" +
+        "4. Salir"
+    );
+
+    switch (opcion) {
+        case "1":
+            agregarProducto();
+            break;
+        case "2":
+            modificarProducto();
+            break;
+        case "3":
+            eliminarProducto();
+            break;
+        case "4":
+            alert(" Saliendo del modo administrador.");
+            break;
+        default:
+            alert(" Opción inválida. Intente de nuevo.");
+        }
+    } while (opcion !== "4");
+}
+
